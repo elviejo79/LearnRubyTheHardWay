f5ad037cf3d2235ec4d6d38c420253e1826106ac
elviejo79 <agarciafdz@gmail.com>
2014-02-02 11:45:59 -0600

    Exercise 2: Take your ex2.rb file and review each line going backwards. Start at the last line, and check each word in reverse against what you should have typed.

--- ex2.rb
+++ ex2.rb
@@ -1,9 +1,9 @@
-# A comment, this is so you can read your programlater.
+# A comment, this is so you can read your program later.
 # Anything afther the # is ignored by Ruby.
 
 puts "I could have code like this." # and the comment after is ginored
 
-# You cand also use a comment to "disabl" or comment out a piece of code:
+# You cand also use a comment to "disable" or comment out a piece of code:
 # puts "This won't run."
 
 puts "This will run."
