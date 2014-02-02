ec06ac713da20bbb3f4f1b4061ccd6f42d960154
elviejo79 <agarciafdz@gmail.com>
2014-02-02 14:38:09 -0600

    Exercise 13: Parameters, Unpacking, Variables

---
+++ ex13.rb
@@ -0,0 +1,7 @@
+first, second, third = ARGV
+
+puts "The script is called: #{$0}"
+puts "Your first value is: #{first}"
+puts "Your second value is #{second}"
+puts "Your third variable is: #{third}"
+
